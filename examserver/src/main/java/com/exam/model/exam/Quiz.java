50b5a94caa3173d1c2bf006eae61053129a56b9c
sakshi-singla-src <108976407+sakshi-singla-src@users.noreply.github.com>
2022-07-10 23:59:48 +0530

    initial setup for exam content
    entity-category,quiz and questions table create from backend

---
+++ examserver/src/main/java/com/exam/model/exam/Quiz.java
@@ -0,0 +1,142 @@
+package com.exam.model.exam;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="quiz")
+public class Quiz {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long qid;
+	
+	private String title;
+	
+	private String description;
+	
+	private String maxMarks;
+	
+	private String numberOfQuestions;
+	
+	private boolean active=false;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Category category;
+	
+	@OneToMany(mappedBy="quiz",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JsonIgnore
+	private Set<Question> questions = new HashSet<>();
+	
+	
+	
+	public Set<Question> getQuestions() {
+		return questions;
+	}
+
+
+	public void setQuestions(Set<Question> questions) {
+		this.questions = questions;
+	}
+
+
+	public Category getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+
+	public Quiz() {
+		super();
+	}
+
+
+	public Quiz(String title, String description, String maxMarks, String numberOfQuestions, boolean active) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.maxMarks = maxMarks;
+		this.numberOfQuestions = numberOfQuestions;
+		this.active = active;
+	}
+
+
+	public Long getQid() {
+		return qid;
+	}
+
+
+	public void setQid(Long qid) {
+		this.qid = qid;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getMaxMarks() {
+		return maxMarks;
+	}
+
+
+	public void setMaxMarks(String maxMarks) {
+		this.maxMarks = maxMarks;
+	}
+
+
+	public String getNumberOfQuestions() {
+		return numberOfQuestions;
+	}
+
+
+	public void setNumberOfQuestions(String numberOfQuestions) {
+		this.numberOfQuestions = numberOfQuestions;
+	}
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+	
+
+}
